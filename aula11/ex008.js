bd1e62eaf2595940e23f06a2889cc285c3716052
Mafalda Santos <92628649+mafaldars@users.noreply.github.com>
2026-09-12 14:29:45 +0100

    if...else

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 40
+
+
+
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+if (vel > 60) { // Condição simples
+    console.log(`Ultrapassou a velocidade permitida. 'Multado(a)!`)
+}
+console.log(`Conduza sempre com precaução.`)
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'USA'
+console.log(`A viver em ${país}.`)
+
+if (país == 'Portugal'){
+    console.log('Português!')
+
+} else {
+    console.log('Estrangeiro')
+}
