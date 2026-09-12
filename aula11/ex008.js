b86d51f443d8bd1c78500d5ce64e5b7e004e909b
Mafalda Santos <92628649+mafaldars@users.noreply.github.com>
2026-09-12 19:20:38 +0100

    Condição simples

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,11 +1,12 @@
-var vel = 40
-
-
-
+var vel = 80
 console.log(`A velocidade do seu carro é ${vel} Km/h.`)
-if (vel > 60) { // Condição simples
-    console.log(`Ultrapassou a velocidade permitida. 'Multado(a)!`)
+
+if (vel > 60) { // condição simples
+    console.log(`Ultrapassou a velocidade permitida! MULTADO(A)!`)
 }
-console.log(`Conduza sempre com precaução.`)
+
+console.log(`Conduza sempre com precaução e use o cinto de segurança!`)
+
+
 
 
